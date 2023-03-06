a02a934cbec4666624d9682ff42dd597699b7da7
dungnnh <dung.nnh5930@sinhvien.hoasen.edu.vn>
2023-03-06 16:43:56 +0700

    tao trang home, marketplace, lobby

--- src/app/pages/loppy/loppy/loppy.module.ts
+++ src/app/pages/loppy/loppy/loppy.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoppyRoutingModule } from './loppy-routing.module';
 import { LoppyComponent } from './loppy.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { LoppyComponent } from './loppy.component';
   ],
   imports: [
     CommonModule,
-    LoppyRoutingModule
+    LoppyRoutingModule,
+    SharedModule,
   ]
 })
 export class LoppyModule { }
